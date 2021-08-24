21fe885351147913c761bccf1a8e7ac48610d211
Your Name <you@example.com>
2021-08-24 17:22:31 +0800

    init Set stage and base 3d object

--- src/Examples.tsx
+++ src/Examples.tsx
@@ -1,16 +1,17 @@
 import React, { useRef, useEffect } from 'react';
 import { render } from 'react-dom';
+import FloppyStage from './FloppyStage';
 
 const Example = () => {
 
   const containerEl = useRef();
 
   useEffect(() => {
-
+    new FloppyStage();
   }, []);
 
   return (
-    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden'}}>Test</div>
+    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden'}}></div>
   )
 }
 
