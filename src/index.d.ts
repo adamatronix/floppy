02f94b154260eac8a2cee26d6085e01c9c63c23b
Your Name <you@example.com>
2021-10-28 12:23:34 +0800

    Updates

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+import FloppyStage from './FloppyStage';
+import FloppyObject from './FloppyObject';
+import loadTextures from './utils/loadTextures';
+export { FloppyStage, FloppyObject, loadTextures };
