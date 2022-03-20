3f4b6b55ed0fe341aff79b21f10715a9d2fd1e03
adamatronix <you@example.com>
2022-03-20 14:48:13 +0800

    floppy-album Include Album in the export and update package version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import FloppyStage from './FloppyStage';
 import FloppyObject from './FloppyObject';
+import FloppyAlbum from './FloppyAlbum';
 import loadTextures from './utils/loadTextures';
 
 export {
   FloppyStage,
   FloppyObject,
+  FloppyAlbum,
   loadTextures
 }
